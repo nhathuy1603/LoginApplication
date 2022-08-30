52d6cd28481642b2bff81d1ccd1c936f7a0178a3
nhathuy1603 <nhathuy1614@gmail.com>
2022-08-30 20:42:10 +0700

    up soursecode

---
+++ app/src/main/java/com/example/exam_1/ui_Login.java
@@ -0,0 +1,14 @@
+package com.example.exam_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ui_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_login);
+    }
+}
